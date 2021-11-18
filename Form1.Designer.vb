fe069f6ea1b4f1839f32cb2364e449dc8edab6ba
naman <namx05@gmail.com>
2021-11-18 18:33:43 +0530

    added new files

---
+++ Form1.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.p1turn = New System.Windows.Forms.Label()
+        Me.p2turn = New System.Windows.Forms.Label()
+        Me.p1score = New System.Windows.Forms.Label()
+        Me.p2score = New System.Windows.Forms.Label()
+        Me.score1 = New System.Windows.Forms.Label()
+        Me.score2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(60, 177)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 42)
+        Me.Button1.TabIndex = 0
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(148, 177)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 42)
+        Me.Button2.TabIndex = 1
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(229, 177)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 42)
+        Me.Button3.TabIndex = 2
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(60, 225)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 43)
+        Me.Button4.TabIndex = 3
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(148, 225)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 43)
+        Me.Button5.TabIndex = 4
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(229, 225)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 43)
+        Me.Button6.TabIndex = 5
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(60, 274)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(75, 45)
+        Me.Button7.TabIndex = 6
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(148, 274)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(75, 45)
+        Me.Button8.TabIndex = 7
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(229, 274)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(75, 45)
+        Me.Button9.TabIndex = 8
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(148, 340)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(75, 23)
+        Me.Button10.TabIndex = 9
+        Me.Button10.Text = "New Round"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'p1turn
+        '
+        Me.p1turn.AutoSize = True
+        Me.p1turn.Location = New System.Drawing.Point(100, 27)
+        Me.p1turn.Name = "p1turn"
+        Me.p1turn.Size = New System.Drawing.Size(35, 13)
+        Me.p1turn.TabIndex = 10
+        Me.p1turn.Text = "'s turn"
+        '
+        'p2turn
+        '
+        Me.p2turn.AutoSize = True
+        Me.p2turn.Location = New System.Drawing.Point(269, 27)
+        Me.p2turn.Name = "p2turn"
+        Me.p2turn.Size = New System.Drawing.Size(35, 13)
+        Me.p2turn.TabIndex = 11
+        Me.p2turn.Text = "'s turn"
+        '
+        'p1score
+        '
+        Me.p1score.AutoSize = True
+        Me.p1score.Location = New System.Drawing.Point(401, 177)
+        Me.p1score.Name = "p1score"
+        Me.p1score.Size = New System.Drawing.Size(43, 13)
+        Me.p1score.TabIndex = 12
+        Me.p1score.Text = "'s score"
+        '
+        'p2score
+        '
+        Me.p2score.AutoSize = True
+        Me.p2score.Location = New System.Drawing.Point(401, 255)
+        Me.p2score.Name = "p2score"
+        Me.p2score.Size = New System.Drawing.Size(43, 13)
+        Me.p2score.TabIndex = 13
+        Me.p2score.Text = "'s score"
+        '
+        'score1
+        '
+        Me.score1.AutoSize = True
+        Me.score1.Location = New System.Drawing.Point(497, 177)
+        Me.score1.Name = "score1"
+        Me.score1.Size = New System.Drawing.Size(13, 13)
+        Me.score1.TabIndex = 14
+        Me.score1.Text = "0"
+        '
+        'score2
+        '
+        Me.score2.AutoSize = True
+        Me.score2.Location = New System.Drawing.Point(497, 255)
+        Me.score2.Name = "score2"
+        Me.score2.Size = New System.Drawing.Size(13, 13)
+        Me.score2.TabIndex = 15
+        Me.score2.Text = "0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(630, 466)
+        Me.Controls.Add(Me.score2)
+        Me.Controls.Add(Me.score1)
+        Me.Controls.Add(Me.p2score)
+        Me.Controls.Add(Me.p1score)
+        Me.Controls.Add(Me.p2turn)
+        Me.Controls.Add(Me.p1turn)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+    Friend WithEvents p1turn As System.Windows.Forms.Label
+    Friend WithEvents p2turn As System.Windows.Forms.Label
+    Friend WithEvents p1score As System.Windows.Forms.Label
+    Friend WithEvents p2score As System.Windows.Forms.Label
+    Friend WithEvents score1 As System.Windows.Forms.Label
+    Friend WithEvents score2 As System.Windows.Forms.Label
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,431 @@
+﻿Public Class Form1
+
+    Public ply1, ply2 As String
+
+    Private Sub Button6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button6.Click
+        If p1turn.Visible = True Then
+            Button6.Text = "O"
+            txt_chk()
+            p1turn.Visible = False
+            p2turn.Visible = True
+            GoTo 50000
+        End If
+
+        If p2turn.Visible = True Then
+            Button6.Text = "X"
+            txt_chk()
+            p1turn.Visible = True
+            p2turn.Visible = False
+            GoTo 50000
+        End If
+
+50000:  Button6.Enabled = False
+    End Sub
+
+    Private Sub Button10_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button10.Click
+        Button1.Text = ""
+        Button1.Enabled = True
+        Button2.Text = ""
+        Button2.Enabled = True
+        Button3.Text = ""
+        Button3.Enabled = True
+        Button4.Text = ""
+        Button4.Enabled = True
+        Button5.Text = ""
+        Button5.Enabled = True
+        Button6.Text = ""
+        Button6.Enabled = True
+        Button7.Text = ""
+        Button7.Enabled = True
+        Button8.Text = ""
+        Button8.Enabled = True
+        Button9.Text = ""
+        Button9.Enabled = True
+    End Sub
+
+    Private Sub Label1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles p1turn.Click
+
+    End Sub
+
+    Private Sub Label3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles p1score.Click
+
+    End Sub
+
+    Private Sub Label6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles score2.Click
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        MsgBox("Welcome to TicTacToe Game by Naman Jain")
+        ply1 = InputBox("Enter Name of player1 ", "Name of player 1")
+        ply2 = InputBox("Enter Name of player2 ", "Name of player 2")
+        If ply1 = "" Then
+            ply1 = "player 1"
+            ply2 = "player 2"
+        End If
+        p1turn.Text = ply1 + p1turn.Text
+        p2turn.Text = ply2 + p2turn.Text
+        p1score.Text = ply1 + p1score.Text
+        p2score.Text = ply2 + p2score.Text
+        p2turn.Visible = False
+    End Sub
+    Private Sub txt_chk()
+        'player2's Win chances'
+        If Button1.Text = "X" And Button2.Text = "X" And Button3.Text = "X" Then
+            MsgBox("Congratulations " & ply2 & ", you win", MsgBoxStyle.Information, "Win")
+            score2.Text += 1
+            Button1.Enabled = False
+            Button2.Enabled = False
+            Button3.Enabled = False
+            Button4.Enabled = False
+            Button5.Enabled = False
+            Button6.Enabled = False
+            Button7.Enabled = False
+            Button8.Enabled = False
+            Button9.Enabled = False
+        ElseIf Button1.Text = "X" And Button5.Text = "X" And Button9.Text = "X" Then
+            MsgBox("Congratulations " & ply2 & ", you win", MsgBoxStyle.Information, "Win")
+            score2.Text += 1
+            Button1.Enabled = False
+            Button2.Enabled = False
+            Button3.Enabled = False
+            Button4.Enabled = False
+            Button5.Enabled = False
+            Button6.Enabled = False
+            Button7.Enabled = False
+            Button8.Enabled = False
+            Button9.Enabled = False
+        ElseIf Button1.Text = "X" And Button4.Text = "X" And Button7.Text = "X" Then
+            MsgBox("Congratulations " & ply2 & ", you win", MsgBoxStyle.Information, "Win")
+            score2.Text += 1
+            Button1.Enabled = False
+            Button2.Enabled = False
+            Button3.Enabled = False
+            Button4.Enabled = False
+            Button5.Enabled = False
+            Button6.Enabled = False
+            Button7.Enabled = False
+            Button8.Enabled = False
+            Button9.Enabled = False
+        ElseIf Button4.Text = "X" And Button5.Text = "X" And Button6.Text = "X" Then
+            MsgBox("Congratulations " & ply2 & ", you win", MsgBoxStyle.Information, "Win")
+            score2.Text += 1
+            Button1.Enabled = False
+            Button2.Enabled = False
+            Button3.Enabled = False
+            Button4.Enabled = False
+            Button5.Enabled = False
+            Button6.Enabled = False
+            Button7.Enabled = False
+            Button8.Enabled = False
+            Button9.Enabled = False
+        ElseIf Button7.Text = "X" And Button8.Text = "X" And Button9.Text = "X" Then
+            MsgBox("Congratulations " & ply2 & ", you win", MsgBoxStyle.Information, "Win")
+            score2.Text += 1
+            Button1.Enabled = False
+            Button2.Enabled = False
+            Button3.Enabled = False
+            Button4.Enabled = False
+            Button5.Enabled = False
+            Button6.Enabled = False
+            Button7.Enabled = False
+            Button8.Enabled = False
+            Button9.Enabled = False
+        ElseIf Button2.Text = "X" And Button5.Text = "X" And Button8.Text = "X" Then
+            MsgBox("Congratulations " & ply2 & ", you win", MsgBoxStyle.Information, "Win")
+            score2.Text += 1
+            Button1.Enabled = False
+            Button2.Enabled = False
+            Button3.Enabled = False
+            Button4.Enabled = False
+            Button5.Enabled = False
+            Button6.Enabled = False
+            Button7.Enabled = False
+            Button8.Enabled = False
+            Button9.Enabled = False
+        ElseIf Button3.Text = "X" And Button6.Text = "X" And Button9.Text = "X" Then
+            MsgBox("Congratulations " & ply2 & ", you win", MsgBoxStyle.Information, "Win")
+            score2.Text += 1
+            Button1.Enabled = False
+            Button2.Enabled = False
+            Button3.Enabled = False
+            Button4.Enabled = False
+            Button5.Enabled = False
+            Button6.Enabled = False
+            Button7.Enabled = False
+            Button8.Enabled = False
+            Button9.Enabled = False
+        ElseIf Button3.Text = "X" And Button5.Text = "X" And Button7.Text = "X" Then
+            MsgBox("Congratulations " & ply2 & ", you win", MsgBoxStyle.Information, "Win")
+            score2.Text += 1
+            Button1.Enabled = False
+            Button2.Enabled = False
+            Button3.Enabled = False
+            Button4.Enabled = False
+            Button5.Enabled = False
+            Button6.Enabled = False
+            Button7.Enabled = False
+            Button8.Enabled = False
+            Button9.Enabled = False
+        End If
+
+        'Player1's Win'
+
+        If Button1.Text = "O" And Button2.Text = "O" And Button3.Text = "O" Then
+            MsgBox("Congratulations " & ply1 & ", you win", MsgBoxStyle.Information, "Win")
+            score1.Text += 1
+            Button1.Enabled = False
+            Button2.Enabled = False
+            Button3.Enabled = False
+            Button4.Enabled = False
+            Button5.Enabled = False
+            Button6.Enabled = False
+            Button7.Enabled = False
+            Button8.Enabled = False
+            Button9.Enabled = False
+        ElseIf Button1.Text = "O" And Button5.Text = "O" And Button9.Text = "O" Then
+            MsgBox("Congratulations " & ply1 & ", you win", MsgBoxStyle.Information, "Win")
+            score1.Text += 1
+            Button1.Enabled = False
+            Button2.Enabled = False
+            Button3.Enabled = False
+            Button4.Enabled = False
+            Button5.Enabled = False
+            Button6.Enabled = False
+            Button7.Enabled = False
+            Button8.Enabled = False
+            Button9.Enabled = False
+        ElseIf Button1.Text = "O" And Button4.Text = "O" And Button7.Text = "O" Then
+            MsgBox("Congratulations " & ply1 & ", you win", MsgBoxStyle.Information, "Win")
+            score1.Text += 1
+            Button1.Enabled = False
+            Button2.Enabled = False
+            Button3.Enabled = False
+            Button4.Enabled = False
+            Button5.Enabled = False
+            Button6.Enabled = False
+            Button7.Enabled = False
+            Button8.Enabled = False
+            Button9.Enabled = False
+        ElseIf Button4.Text = "O" And Button5.Text = "O" And Button6.Text = "O" Then
+            MsgBox("Congratulations " & ply1 & ", you win", MsgBoxStyle.Information, "Win")
+            score1.Text += 1
+            Button1.Enabled = False
+            Button2.Enabled = False
+            Button3.Enabled = False
+            Button4.Enabled = False
+            Button5.Enabled = False
+            Button6.Enabled = False
+            Button7.Enabled = False
+            Button8.Enabled = False
+            Button9.Enabled = False
+        ElseIf Button7.Text = "O" And Button8.Text = "O" And Button9.Text = "O" Then
+            MsgBox("Congratulations " & ply1 & ", you win", MsgBoxStyle.Information, "Win")
+            score1.Text += 1
+            Button1.Enabled = False
+            Button2.Enabled = False
+            Button3.Enabled = False
+            Button4.Enabled = False
+            Button5.Enabled = False
+            Button6.Enabled = False
+            Button7.Enabled = False
+            Button8.Enabled = False
+            Button9.Enabled = False
+        ElseIf Button2.Text = "O" And Button5.Text = "O" And Button8.Text = "O" Then
+            MsgBox("Congratulations " & ply1 & ", you win", MsgBoxStyle.Information, "Win")
+            score1.Text += 1
+            Button1.Enabled = False
+            Button2.Enabled = False
+            Button3.Enabled = False
+            Button4.Enabled = False
+            Button5.Enabled = False
+            Button6.Enabled = False
+            Button7.Enabled = False
+            Button8.Enabled = False
+            Button9.Enabled = False
+        ElseIf Button3.Text = "O" And Button6.Text = "O" And Button9.Text = "O" Then
+            MsgBox("Congratulations " & ply1 & ", you win", MsgBoxStyle.Information, "Win")
+            score1.Text += 1
+            Button1.Enabled = False
+            Button2.Enabled = False
+            Button3.Enabled = False
+            Button4.Enabled = False
+            Button5.Enabled = False
+            Button6.Enabled = False
+            Button7.Enabled = False
+            Button8.Enabled = False
+            Button9.Enabled = False
+        ElseIf Button3.Text = "O" And Button5.Text = "O" And Button7.Text = "O" Then
+            MsgBox("Congratulations " & ply1 & ", you win", MsgBoxStyle.Information, "Win")
+            score1.Text += 1
+            Button1.Enabled = False
+            Button2.Enabled = False
+            Button3.Enabled = False
+            Button4.Enabled = False
+            Button5.Enabled = False
+            Button6.Enabled = False
+            Button7.Enabled = False
+            Button8.Enabled = False
+            Button9.Enabled = False
+        End If
+    End Sub
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If p1turn.Visible = True Then
+            Button1.Text = "O"
+            txt_chk()
+            p1turn.Visible = False
+            p2turn.Visible = True
+            GoTo 50000
+        End If
+
+        If p2turn.Visible = True Then
+            Button1.Text = "X"
+            txt_chk()
+            p1turn.Visible = True
+            p2turn.Visible = False
+            GoTo 50000
+        End If
+
+50000:  Button1.Enabled = False
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        If p1turn.Visible = True Then
+            Button2.Text = "O"
+            txt_chk()
+            p1turn.Visible = False
+            p2turn.Visible = True
+            GoTo 50000
+        End If
+
+        If p2turn.Visible = True Then
+            Button2.Text = "X"
+            txt_chk()
+            p1turn.Visible = True
+            p2turn.Visible = False
+            GoTo 50000
+        End If
+
+50000:  Button2.Enabled = False
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        If p1turn.Visible = True Then
+            Button3.Text = "O"
+            txt_chk()
+            p1turn.Visible = False
+            p2turn.Visible = True
+            GoTo 50000
+        End If
+
+        If p2turn.Visible = True Then
+            Button3.Text = "X"
+            txt_chk()
+            p1turn.Visible = True
+            p2turn.Visible = False
+            GoTo 50000
+        End If
+
+50000:  Button3.Enabled = False
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        If p1turn.Visible = True Then
+            Button4.Text = "O"
+            txt_chk()
+            p1turn.Visible = False
+            p2turn.Visible = True
+            GoTo 50000
+        End If
+
+        If p2turn.Visible = True Then
+            Button4.Text = "X"
+            txt_chk()
+            p1turn.Visible = True
+            p2turn.Visible = False
+            GoTo 50000
+        End If
+
+50000:  Button4.Enabled = False
+    End Sub
+
+    Private Sub Button5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button5.Click
+        If p1turn.Visible = True Then
+            Button5.Text = "O"
+            txt_chk()
+            p1turn.Visible = False
+            p2turn.Visible = True
+            GoTo 50000
+        End If
+
+        If p2turn.Visible = True Then
+            Button5.Text = "X"
+            txt_chk()
+            p1turn.Visible = True
+            p2turn.Visible = False
+            GoTo 50000
+        End If
+
+50000:  Button5.Enabled = False
+    End Sub
+
+    Private Sub Button7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button7.Click
+        If p1turn.Visible = True Then
+            Button7.Text = "O"
+            txt_chk()
+            p1turn.Visible = False
+            p2turn.Visible = True
+            GoTo 50000
+        End If
+
+        If p2turn.Visible = True Then
+            Button7.Text = "X"
+            txt_chk()
+            p1turn.Visible = True
+            p2turn.Visible = False
+            GoTo 50000
+        End If
+
+50000:  Button7.Enabled = False
+    End Sub
+
+    Private Sub Button8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button8.Click
+        If p1turn.Visible = True Then
+            Button8.Text = "O"
+            txt_chk()
+            p1turn.Visible = False
+            p2turn.Visible = True
+            GoTo 50000
+        End If
+
+        If p2turn.Visible = True Then
+            Button8.Text = "X"
+            txt_chk()
+            p1turn.Visible = True
+            p2turn.Visible = False
+            GoTo 50000
+        End If
+
+50000:  Button8.Enabled = False
+    End Sub
+
+    Private Sub Button9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button9.Click
+        If p1turn.Visible = True Then
+            Button9.Text = "O"
+            txt_chk()
+            p1turn.Visible = False
+            p2turn.Visible = True
+            GoTo 50000
+        End If
+
+        If p2turn.Visible = True Then
+            Button9.Text = "X"
+            txt_chk()
+            p1turn.Visible = True
+            p2turn.Visible = False
+            GoTo 50000
+        End If
+
+50000:  Button9.Enabled = False
+    End Sub
+End Class
